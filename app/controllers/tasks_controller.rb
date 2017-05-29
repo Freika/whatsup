45197dbf50926ae6863c0d9f8d29a5e5bc4b0548
Eugene Burmakin <iamfrey@gmail.com>
2017-05-29 15:44:42 +0700

    Add tasks forms to index.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @done_tasks = Task.done
-    @todo_tasks = Task.todo
-    @backlog_tasks = Task.backlog
+    load_tasks
   end
 
   def show
@@ -19,7 +17,8 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
 
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created.'
+      load_tasks
+      render :index
     else
       render :new
     end
@@ -48,5 +47,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(
       :content, :status, :estimated_hours, :spent_hours
     )
+  end
+
+  def load_tasks
+    @done_tasks = Task.done
+    @todo_tasks = Task.todo
+    @backlog_tasks = Task.backlog
   end
 end
